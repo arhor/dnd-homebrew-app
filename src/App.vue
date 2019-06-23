43779044aaf821623a984b8e3e21b53b5be42368
Maksim Buryshynets <Maksim_Buryshunets@epam.com>
2019-06-23 12:06:06 +0300

    dependencies refactored; appData propert added;

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,27 @@
 <template>
   <v-app>
-
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span class="font-weight-light">{{ app_name }}</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
       <v-btn to="/" flat>Home</v-btn>
       <v-btn to="/about" flat>About</v-btn>
       <v-btn to="/dev" flat>Dev</v-btn>
     </v-toolbar>
-
     <v-content>
       <router-view/>
     </v-content>
-
   </v-app>
 </template>
 
 <script>
+import store from './store.js';
+
 export default {
   name: 'App',
+  computed: {
+    app_name: () => store.appData.name,
+  }
 };
 </script>
--- src/store.js
+++ src/store.js
@@ -2,4 +2,7 @@ import axios from 'axios';
 
 export default {
   findMonsters: () => axios.get('data/5e-SRD-Monsters.json'),
+  appData: {
+    name: 'D&D Homebrew App',
+  },
 };
