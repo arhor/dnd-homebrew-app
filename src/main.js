0463f9a473c193990d927d8842c15965ec5bed0b
arhor <RZAB9qkn>
2020-02-02 13:34:27 +0300

    version updates;

--- src/main.js
+++ src/main.js
@@ -2,12 +2,13 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store/index';
-import './plugins/vuetify';
+import vuetify from './plugins/vuetify';
 import './assets/style/main.scss';
 
 Vue.config.productionTip = false;
 
 new Vue({
+  vuetify,
   router,
   store,
   render: h => h(App),
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,14 @@
 import Vue from 'vue';
-import Vuetify from 'vuetify/lib';
-import 'vuetify/src/stylus/app.styl';
+import Vuetify, { VLayout } from 'vuetify/lib';
 
 Vue.use(Vuetify, {
-  iconfont: 'md',
+  components: {
+    VLayout,
+  },
+});
+
+export default new Vuetify({
+  icons: {
+    iconfont: 'mdi',
+  },
 });
